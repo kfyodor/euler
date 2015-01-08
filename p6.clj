b8d2b9d1cf9b234a20cffa301e972e88583198e2
Theodore Konukhov <komolov.f@gmail.com>
2015-01-08 04:40:03 +0300

    problem 6

--- p6.clj
+++ p6.clj
@@ -1,1 +1,16 @@
 (ns euler.p6)
+
+(defn solve-naive [n]
+  "Straightforward approach"
+  (let [sum-of-squares (reduce + (map #(* % %) (range 1 (inc n))))
+        sum            (reduce + (range 1 (inc n)))
+        square-of-sum  (* sum sum)]
+    (- square-of-sum sum-of-squares)))
+
+(defn solve [n]
+  "http://en.wikipedia.org/wiki/Square_number
+   http://mathcentral.uregina.ca/QQ/database/QQ.02.06/jo1.html"
+  (let [sum-of-squares (/ (* n (inc n) (inc (* 2 n))) 6)
+        sum            (/ (* n (inc n)) 2)
+        square-of-sum  (* sum sum)]
+    (- square-of-sum sum-of-squares)))
